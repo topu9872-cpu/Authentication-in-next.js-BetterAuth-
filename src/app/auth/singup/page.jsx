a91f081ecdc26f544bf81f26c050ae7ca1cf5074
topu9872-cpu <topu9872@gmail.com>
2026-04-24 20:09:53 +0600

    fixed singup issue

--- src/app/auth/singup/page.jsx
+++ src/app/auth/singup/page.jsx
@@ -1,40 +1,56 @@
 "use client";
 import { authClient } from "@/lib/auth-client";
-import { Check } from "@gravity-ui/icons";
+import { Check, Eye, EyeSlash } from "@gravity-ui/icons";
 import {
   Button,
   Description,
   FieldError,
   Form,
   Input,
+  InputGroup,
   Label,
   TextField,
 } from "@heroui/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const SingUpPage = () => {
-  const onSubmit = async(e) => {
-    e.preventDefault();
-    const formData=new FormData(e.currentTarget);
-const userData= Object.fromEntries(formData.entries())
-console.log(userData);
-const {data,error}=await authClient.signUp.email({
+  const router = useRouter();
 
-name:userData.name,
-email:userData.email,
-password:userData.password,
-callbackURL:'/auth/singin'
-})
-/**
- * ! conditinoal is opsnol we can use tostify 
- */
-   console.log(data,error)
-   if(error){
-    alert('Sing Up response:' + error.message)
-   }
-   if(data){
-    alert('Sign Up Successful ! Please Cheke your email Verify your account .')
-   }
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const userData = Object.fromEntries(formData.entries());
+    console.log(userData);
+    const { data, error } = await authClient.signUp.email(
+      {
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+        // callbackURL:'/auth/singin'
+      },
+      {
+        onSuccess: () => {
+          router.push("/auth/singin");
+        },
+      },
+    );
+    /**
+     * ! conditinoal is opsnol we can use tostify
+     */
+    console.log(data, error);
+    if (error) {
+      alert("Sing Up response:" + error.message);
+    }
+    if (data) {
+      alert(
+        "Sign Up Successful ! Please Cheke your email Verify your account .",
+      );
+    }
   };
+
+  
+    const [isVisible, setIsVisible] = useState(false);
 
   return (
     <div className=" mx-auto mt-10 ">
@@ -54,7 +70,7 @@ callbackURL:'/auth/singin'
           }}
         >
           <Label>Name</Label>
-          <Input name='name' placeholder="enter your name" />
+          <Input name="name" placeholder="enter your name" />
           <FieldError />
         </TextField>
 
@@ -70,33 +86,33 @@ callbackURL:'/auth/singin'
           }}
         >
           <Label>Email</Label>
-          <Input name='email' placeholder="enter your email" />
+          <Input name="email" placeholder="enter your email" />
           <FieldError />
         </TextField>
-        <TextField
-          isRequired
-          minLength={8}
-          name="password"
-          type="password"
-          validate={(value) => {
-            if (value.length < 8) {
-              return "Password must be at least 8 characters";
-            }
-            if (!/[A-Z]/.test(value)) {
-              return "Password must contain at least one uppercase letter";
-            }
-            if (!/[0-9]/.test(value)) {
-              return "Password must contain at least one number";
-            }
-            return null;
-          }}
-        >
+       <TextField className="w-full max-w-70" name="password">
           <Label>Password</Label>
-          <Input name='password' placeholder="Enter your password" />
-          <Description>
-            Must be at least 8 characters with 1 uppercase and 1 number
-          </Description>
-          <FieldError />
+          <InputGroup>
+            <InputGroup.Input
+              className="w-full max-w-70"
+              type={isVisible ? "text" : "password"}
+              name='password' placeholder="Your Password"
+            />
+            <InputGroup.Suffix className="pr-0">
+              <Button
+                isIconOnly
+                aria-label={isVisible ? "Hide password" : "Show password"}
+                size="sm"
+                variant="ghost"
+                onPress={() => setIsVisible(!isVisible)}
+              >
+                {isVisible ? (
+                  <Eye className="size-4" />
+                ) : (
+                  <EyeSlash className="size-4" />
+                )}
+              </Button>
+            </InputGroup.Suffix>
+          </InputGroup>
         </TextField>
         <div className="flex gap-2">
           <Button type="submit">
